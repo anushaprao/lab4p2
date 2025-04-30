aa681fd350567d839110fa905be5170bb6a33301
anushaprao <122496338+anushaprao@users.noreply.github.com>
2025-04-30 13:02:55 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -29,7 +29,7 @@ function discountPrices(prices, discount) {
     const length = prices.length;
     //let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        const discountedPrice += prices[i] * (1 - discount);
+        const discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
